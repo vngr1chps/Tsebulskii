b39092e05f398a2c453743370546e0d04b902738
Stepan_Tsebulskii <steve.tseb@gmail.com>
2024-09-20 23:30:47 +0300

    random operations

--- homework1.py
+++ homework1.py
@@ -1,4 +1,14 @@
-print('hello world')
-a = int(input('Введите первое число'))
-b = int(input('Введите второе число'))
-print(a + b)
+import random
+a = int(input('Введите первое число '))
+b = int(input('Введите второе число '))
+
+random_choice = random.randint(1, 4)
+
+if random_choice == 1:
+    print(a + b)
+elif random_choice == 2:
+    print(a - b)
+elif random_choice == 3:
+    print(a * b)
+else:
+    print(a / b)
